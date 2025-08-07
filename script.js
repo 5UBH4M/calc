0fba514b04ec36bc3d21847e05b88c39907a079c
Subham9670 <therajpootgame@gmail.com>
2025-08-07 05:44:29 +0000

    change html title

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-let display = document.getElementById("display")
+const display = document.getElementById("display");
 
-function toDisplay(n){
-    display.value += n
+function toDisplay(n) {
+  display.value += n;
 }
 
-function clearDisplay(){
-    display.value = ""
+function clearDisplay() {
+  display.value = "";
 }
 
-function del(){
-    display.value = display.value.slice(0, -1)
+function del() {
+  display.value = display.value.slice(0, -1);
 }
 
-
-function toCalculate(){
-    try{
-        display.value = eval(display.value)
-    }
-    catch(error){
-        display.value = "ERROR"
-    }
+function toCalculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "ERROR";
+  }
 }
