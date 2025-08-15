ab369d743b02d719c424137141b68f7f80dd7660
Subham9670 <therajpootgame@gmail.com>
2025-08-15 18:58:43 +0530

    added function to handle the repetition of operators

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 const display = document.getElementById("display");
+const operators = ['+', '-', '.', '/', '*'];
 
-function toDisplay(n) {
-  display.value += n;
+function toDisplay(n){
+  let lastChar = display.value.slice(-1);
+   
+  if (display.value === "ERROR"){
+    clearDisplay();
+  }
+  
+  switch(n){
+    case "1": display.value += n; break;
+    case "2": display.value += n; break;
+    case "3": display.value += n; break;
+    case "4": display.value += n; break;
+    case "5": display.value += n; break;
+    case "6": display.value += n; break;
+    case "7": display.value += n; break;
+    case "8": display.value += n; break;
+    case "9": display.value += n; break;
+    case "0": display.value += n; break;
+    case "00": display.value += n; break;
+    case "+": toHandleOperators(); display.value += n; break;
+    case "/": toHandleOperators(); display.value += n; break;
+    case "*": toHandleOperators(); display.value += n; break;
+    case "-": toHandleOperators(); display.value += n; break;
+    case ".": toHandleOperators(); display.value += n; break;
+    case "=": toCalculate(); break;
+    default : display.value = "ERROR";
+  }
+
+  // a function to handle the repetition of the operators
+  function toHandleOperators(){
+    if (operators.includes(n) && operators.includes(lastChar)){
+      del();
+    }
+  }
 }
 
 function clearDisplay() {
